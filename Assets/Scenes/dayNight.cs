26988a62ee3ea23e75390b2407ee673ee940b814
SOURYA DEY <142716790+sourya001@users.noreply.github.com>
2025-03-30 12:45:10 +0530

    Update dayNight.cs

--- Assets/Scenes/dayNight.cs
+++ Assets/Scenes/dayNight.cs
@@ -5,14 +5,13 @@ public class ToggleChildButtons : MonoBehaviour
     public void SwitcherMethod()
     {
         Debug.Log("hello");
-        // Ensure there are exactly two child buttons
+      
         if (transform.childCount != 2)
         {
             Debug.LogError("The GameObject must have exactly 2 child buttons.");
             return;
         }
 
-        // Get the first and second child buttons
         GameObject button1 = transform.GetChild(0).gameObject;
         GameObject button2 = transform.GetChild(1).gameObject;
 
